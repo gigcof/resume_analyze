c99b9be7973c611c07ecada2b7749d5b684c4cb6
gigcof <55837706+gigcof@users.noreply.github.com>
2024-03-27 11:46:36 +0530

    Delete resume-analyze directory

--- resume-analyze/server/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-const upload = multer();
-const uploadDataAndGenerateResponse = require('./controllers/uploadController');
-
-app.post('/upload', upload.single('resume'), uploadDataAndGenerateResponse);
